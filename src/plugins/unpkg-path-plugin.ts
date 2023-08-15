209fe8d540e6b31e2ebb95c4327c5db92af9965c
Norbert.Duchnowski <dnaid4k@gmail.com>
2023-08-15 15:54:26 +0200

    test for localForage

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,6 +1,19 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
  
+const fileCache = localForage.createInstance({
+  name: 'filecache'
+});
+
+(async() => {
+  await fileCache.setItem('color', 'red');
+
+  const color = await fileCache.getItem('color');
+
+  console.log(color);
+})()
+
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
@@ -32,8 +45,8 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import React from 'react';
-              console.log(React);
+              import React, { useState } from 'react-select';
+              console.log(React, useState);
             `,
           };
         } 
