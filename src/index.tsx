852650791abc00591e845ae0a6ac8e6c647edc60
Norbert.Duchnowski <dnaid4k@gmail.com>
2023-08-23 10:47:05 +0200

    passing code to the iframe

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState(''); 
   const [code, setCode] = useState(''); 
 
@@ -38,16 +39,24 @@ const App = () => {
       }
     });
     
-    //console.log(result);
+    //setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
 
-    setCode(result.outputFiles[0].text);
-    
-    try {
-    eval(result.outputFiles[0].text);
-    } catch (err) {
-      alert(err);
-    }
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data);
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
   <div>
@@ -56,14 +65,10 @@ const App = () => {
       <button onClick={onClick}>Submit</button>
     </div>
     <pre>{code}</pre>
-    <iframe sandbox="" srcDoc={html}/>
+    <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
   </div>
   );
 };
-
-const html = `
-<h1>Local HTML doc</h1>
-`;
 
 ReactDOM.render(
 <App />,
