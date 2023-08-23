013a5c4ac3fdc9b67e4ce8aa5412ef6da96c78ab
Norbert.Duchnowski <dnaid4k@gmail.com>
2023-08-23 16:21:48 +0200

    catchin errors

--- src/index.tsx
+++ src/index.tsx
@@ -51,7 +51,13 @@ const App = () => {
         <div id="root"></div>
         <script>
           window.addEventListener('message', (event) => {
-            eval(event.data);
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div>' + err + '</div>';
+              console.error(err);
+            }
           }, false);
         </script>
       </body>
